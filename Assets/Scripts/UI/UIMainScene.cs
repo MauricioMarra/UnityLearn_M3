2bbe08d3de88041787c499f7b3c2c5f0e8193666
MauricioMarra <52969819+MauricioMarra@users.noreply.github.com>
2023-03-15 20:14:42 -0300

    feat: adds persistency between scenes and sessions

--- Assets/Scripts/UI/UIMainScene.cs
+++ Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,9 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
